da56b805b68ee371fcbe34e893d6d1317d376fa2
Sonar <Sonar@imacs-imac.local>
2019-02-11 14:25:38 -0500

    Initial Commit

---
+++ Go2Rad/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Go2Rad
+//
+//  Created by Sonar on 2/11/19.
+//  Copyright © 2019 Sonar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Go2RadTests/Go2RadTests.swift
@@ -0,0 +1,34 @@
+//
+//  Go2RadTests.swift
+//  Go2RadTests
+//
+//  Created by Sonar on 2/11/19.
+//  Copyright © 2019 Sonar. All rights reserved.
+//
+
+import XCTest
+@testable import Go2Rad
+
+class Go2RadTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
